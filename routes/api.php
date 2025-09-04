ad23026ff88fd14a053ff6e5ccff913c98b80cff
Hossain Forhad <hossain.28forhad@gmail.com>
2025-09-04 13:38:46 +0600

    Scantum API

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\StudentController;
+use App\Http\Controllers\Api\ApiController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
@@ -10,3 +11,15 @@ use App\Http\Controllers\Api\StudentController;
 
 
 Route::apiResource("students", StudentController::class);
+
+/* Scantum API */
+
+Route::post("register", [ApiController::class, "register"]);
+Route::post("login", [ApiController::class, "login"]);
+
+Route::group(["middleware" => ["auth:sanctum"]], function () {
+    // Route::apiResource("students", StudentController::class);
+
+    Route::get("profile", [ApiController::class, "profile"]);
+    Route::get("logout", [ApiController::class, "logout"]);
+});
